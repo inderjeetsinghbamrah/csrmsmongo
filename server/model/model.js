58970f76fdbcfa4cbca4c9466e0bf49d563c46e0
Inderjeet Singh Bamrah <inderjeetsinghit@gmail.com>
2022-09-08 16:52:35 +0530

    Working on mongodb model

--- server/model/model.js
+++ server/model/model.js
@@ -1,19 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 var schema = new mongoose.Schema({
-    name : {
-        type : String,
-        required: true
-    },
-    email : {
-        type: String,
-        required: true,
-        unique: true
-    },
-    gender : String,
-    status : String
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  father_name: String,
+  mother_name: String,
+  dob: Date,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  mobile: Number,
+  gender: String,
+});
 
-const Userdb = mongoose.model('userdb', schema);
+const student_basic_info = mongoose.model("studentBasicInfo", schema);
 
-module.exports = Userdb;
+module.exports = student_basic_info;
